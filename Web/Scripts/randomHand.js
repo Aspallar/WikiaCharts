38e9b9e9cb38c3d46015b966af0a6258a9a4e8e7
Aspallar <shadowthrone99@outlook.com>
2017-10-30 18:52:25 +0000

    Random hand improvements

--- Web/Scripts/randomHand.js
+++ Web/Scripts/randomHand.js
@@ -1,22 +1,22 @@
 (function ($) {
     'use strict';
-    /*jshint curly: false */
+    /*jshint curly: false, maxlen: 200 */
 
-    var cardImageParseTemplate = 'http://magicduels.wikia.com/api.php?format=json&action=parse&disablepp=true&prop=text&text=%5B%5BFile%3A[[cardname]]%7Csize%3D160px%7Clink%3D%5D%5D';
-    var deck;
+    var randomHandButtonId = 'mdw-random-hand-button';
+    var randomHandResultsId = 'mdw-random-hand';
 
-    function DeckEntry(name, amount) {
-        this.name = name;
-        this.amount = amount;
-        this.available = true;
-        this.imageSource = null;
+    // do nothing on articles with no random hand
+    if (document.getElementById(randomHandButtonId) === null ||
+            document.getElementById(randomHandResultsId) === null) {
+        return; 
     }
 
-    DeckEntry.prototype = {
-        constructor: DeckEntry,
-        encodedName: function () {
-            return encodeURIComponent(this.name);
-        }
+    var deck;
+    var cardImageSources = {};
+
+    function DeckEntry(name) {
+        this.name = name;
+        this.available = true;
     }
 
     function Deck() {
@@ -27,7 +27,7 @@
         constructor: Deck,
         shuffle: function () {
             this.cards.forEach(function (card) {
-                card.available = card.amount;
+                card.available = true;
             });
         },
         drawCard: function () {
@@ -35,9 +35,18 @@
             do {
                 index = Math.floor(Math.random() * this.cards.length);
                 card = this.cards[index];
-            } while (card.available <= 0);
-            --card.available;
+            } while (!card.available);
+            card.available = false;
             return card;
+        },
+        drawCards: function (numCards) {
+            var cards = [];
+            for (var k = 0; k < numCards; k++)
+                cards.push(this.drawCard());
+            cards.sort(function (a, b) {
+                return a.name.localeCompare(b.name);
+            });
+            return cards;
         },
         scrapeFromPage: function () {
             var deck = [];
@@ -45,43 +54,56 @@
             cardElements.each(function () {
                 var name = $(this).text();
                 var amount = parseInt($.trim(this.previousSibling.textContent), 10);
-                if (isNaN(amount))
-                    amount = 0;
-                deck.push(new DeckEntry(name, amount));
+                if (!isNaN(amount)) {
+                    for (var k = 0; k < amount; k++)
+                        deck.push(new DeckEntry(name));
+                }
             });
             this.cards = deck;
-         }
+        }
     };
 
     function setCardImage(img, card) {
-        if (card.imageSource !== null) {
-            img.attr('src', card.imageSource);
+        var imageSource = cardImageSources[card.name];
+        if (imageSource !== undefined) {
+            img.attr('src', imageSource);
             return;
         }
-        var url = cardImageParseTemplate.replace("[[cardname]]", encodeURIComponent(card.name) + '.png');
+        var template = 'http://magicduels.wikia.com/api.php?format=json&action=parse&disablepp=true&prop=text&text=%5B%5BFile%3A[[cardname]].png%7Csize%3D160px%7Clink%3D%5D%5D';
+        var url = template.replace('[[cardname]]', encodeURIComponent(card.name));
         $.getJSON(url, function (data) {
             var text = data.parse.text['*'];
-            var match = /src\s*=\s*"([^"]+)"/.exec(text);
-            card.imageSource = match[1];
-            img.attr('src', match[1]);
-        })
+            var sourceMatch = /src\s*=\s*"([^"]+)"/.exec(text);  
+            cardImageSources[card.name] = sourceMatch[1];
+            img.attr('src', sourceMatch[1]);
+        });
+    }
+
+    function renderRandomHand(target, hand) {
+        var cardElements = document.createDocumentFragment();
+        hand.forEach(function (card) {
+            var img = $('<img></img>').attr('width', 223).attr('height', 311);
+            cardElements.appendChild(img.get(0));
+            setCardImage(img, card);
+        });
+        target.html(cardElements);
+    }
+
+    function generateRandomHand() {
+        var randomHandDiv = $('#' + randomHandResultsId);
+        if (deck.cards.length < 7) {
+            randomHandDiv.html('<p>There must be at least seven cards in a deck for a random hand.</p>');
+            return;
+        }
+        deck.shuffle();
+        var hand = deck.drawCards(7);
+        renderRandomHand(randomHandDiv, hand);
     }
 
     $(document).ready(function () {
         deck = new Deck();
         deck.scrapeFromPage();
-        $('#mdw-random-hand-button').click(function () {
-            var randomHandDiv = $('#mdw-random-hand');
-            randomHandDiv.html('');
-            deck.shuffle();
-            var hand = [];
-            for (var k = 0; k < 7; k++) {
-                var card = deck.drawCard();
-                var img = $("<img></img>");
-                randomHandDiv.append(img);
-                setCardImage(img, card);
-            }
-        });
+        $('#' + randomHandButtonId).click(generateRandomHand);
     });
 })(jQuery);
 
